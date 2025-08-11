3ead0f1cfc60ad7a86bd8b0451fab323006c5328
Abhiram302 <abhiramburra3@gmail.com>
2025-08-11 20:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const images = document.querySelectorAll(".image")
+
+for(let i = 0; i<images.length; i++){
+	images[i].addEventListener("dragstart",()=>{
+		let image_link = images[i]
+	})
+	images[i].addEventListener("dragover",(e)=>{
+		e.preventDefault()
+	})
+	images[i].addEventListener("drop",(e)=>{
+		let dropOnImage = e.target.style.backgroundImage
+		let dragedImage = image_link.style.backgroundImage
+		e.target.style.backgroundImage =  dragedImage
+		image_link.style.backgroundImage = dropOnImage
+	})
+}
